ab49135c44d2f367cd39b8b64764da3a31cc9ab0
Jacob <jaw476@cornell.edu>
2022-03-20 12:23:06 -0400

    Ingredient

--- core/src/edu/cornell/gdiac/molechelinmadness/model/Ingredient.java
+++ core/src/edu/cornell/gdiac/molechelinmadness/model/Ingredient.java
@@ -4,6 +4,25 @@ import edu.cornell.gdiac.molechelinmadness.model.obstacle.BoxObstacle;
 
 public class Ingredient extends BoxObstacle {
 
+    public enum IngType {
+        CARROT,
+        TURNIP,
+        RADISH
+    }
+    //Type of ingredient
+    public IngType type;
+
+
+    //Gets the ingredients type
+    public IngType getIngredientType(){
+        return this.type;
+    }
+
+
+    //Sets the ingredients type
+    public void setIngType(IngType ing){
+        this.type = ing;
+    }
 
     /**
      * Creates a new box at the origin.
@@ -15,7 +34,9 @@ public class Ingredient extends BoxObstacle {
      * @param width  The object width in physics units
      * @param height The object width in physics units
      */
-    public Ingredient(float width, float height) {
-        super(width, height);
+    public Ingredient(float x, float y, float width, float height, IngType type) {
+        super(x, y, width, height);
+        this.type = type;
+
     }
 }
